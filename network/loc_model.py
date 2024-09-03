423ae2f10024130685fdbe3a86bea46e1d8ecd0f
feiyue <fyue0112@163.com>
2024-09-03 09:24:09 +0800

    add background print and solve a package import bug.

--- network/loc_model.py
+++ network/loc_model.py
@@ -23,6 +23,7 @@ class LitelocModel:
 
         if params.Training.bg is None:
             params.Training.bg = calculate_bg(params.Training.infer_data)
+            print('training background is: ' + str(params.Training.bg))
 
         self.DataGen = DataGenerator(params.Training, params.Camera, params.PSF_model)
 
@@ -85,7 +86,7 @@ class LitelocModel:
             tt = time.time()
             local_context = True
             for i in range(0, self.params.Training.eval_iteration):
-                # todo: delete unused functions
+
                 locs, X, Y, Z, I, s_mask, xyzi_gt = self.DataGen.generate_batch_newest(self.params.Training.batch_size, val=False, local_context=local_context)
 
                 imgs_sim = self.DataGen.simulate_image(s_mask, xyzi_gt, locs, X, Y, Z, I)
