1e2c38f9a0d6471c4422032fe8079c33a77cf9f4
feiyue <fyue0112@163.com>
2024-10-03 19:57:49 +0800

    Update the interface of loading spline PSF model file.

--- spline_psf/calibration_io.py
+++ spline_psf/calibration_io.py
@@ -13,8 +13,11 @@ class SMAPSplineCoefficient:
             file:
         """
         self.calib_file = calib_file
-        self.calib_mat = sio.loadmat(self.calib_file, struct_as_record=False, squeeze_me=True)['cspline_psf_model']  # todo: 'cspline_model' is not used
-
+        calib_mat = sio.loadmat(self.calib_file, struct_as_record=False, squeeze_me=True)
+        if 'cspline_psf_model' in calib_mat.keys():
+            self.calib_mat = calib_mat['cspline_psf_model']
+        else:
+            self.calib_mat = calib_mat['SXY'].cspline
         self.coeff = torch.from_numpy(self.calib_mat.coeff)
         self.ref0 = (self.calib_mat.x0 - 1, self.calib_mat.x0 - 1, self.calib_mat.z0)
         self.dz = self.calib_mat.dz
