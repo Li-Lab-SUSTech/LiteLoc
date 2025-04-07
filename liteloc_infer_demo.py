7ec3f2c93548ae3b47e30a083ce76495fb1fe8c0
SpikeRXWong <60434187+SpikeRXWong@users.noreply.github.com>
2025-04-07 10:34:28 +0800

    general training & inference code
    codes for training and inference, same code with argparse package to control the inputs

--- liteloc_infer_demo.py
+++ liteloc_infer_demo.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # yaml_file = 'infer_params_demo_fig3a.yaml'  # remember to change p probability
     infer_params = load_yaml_infer(args.infer_params_path)
 
-    test_model = torch.load(infer_params.Loc_Model.model_path) # suitable for both DECODE and LiteLoc 
+    infer_model = torch.load(infer_params.Loc_Model.model_path) # suitable for both DECODE and LiteLoc 
 
     multi_process_params = infer_params.Multi_Process
 
@@ -30,16 +30,16 @@ if __name__ == '__main__':
     
 
     liteloc_analyzer = multi_process.CompetitiveSmlmDataAnalyzer_multi_producer(
-        loc_model=test_model,
+        loc_model=infer_model,
         tiff_path=multi_process_params.image_path,
         output_path=multi_process_params.save_path,
         time_block_gb=multi_process_params.time_block_gb,
         batch_size=multi_process_params.batch_size,
         sub_fov_size=multi_process_params.sub_fov_size,
         over_cut=multi_process_params.over_cut,
-        multi_GPU=multi_process_params.multi_gpu,
-        end_frame_num=multi_process_params.end_frame_num,
-        num_producers=multi_process_params.num_producers,
+        multi_GPU=multi_process_params.multi_gpu, 
+        end_frame_num = multi_process_params.end_frame_num if 'end_frame_num' in multi_process_params else None, #
+        num_producers=multi_process_params.num_producers, #
     )
 
     torch.cuda.synchronize()
--- liteloc_train_demo.py
+++ liteloc_train_demo.py
@@ -1,11 +1,11 @@
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ['CUDA_VISIBLE_DEVICES'] = "3"
+# os.environ['CUDA_VISIBLE_DEVICES'] = "3"
 
 import argparse
 from utils.help_utils import load_yaml_train, writelog, setup_seed
 from utils.visual_utils import show_sample_psf, show_train_img
-
+# from network.loc_model_decode import LocModel
 
 if __name__ == '__main__':
 
@@ -13,22 +13,22 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model_name', type = str, default = 'LiteLoc', choices = ['LiteLoc', 'DECODE'])
+    parser.add_argument('-n', '--gpu_device_num', type = int, default = 1)
     parser.add_argument('-p', '--train_params_path', type=str, default='train_params_demo_fig3a.yaml')
     args = parser.parse_args()
     
-    # if args.model_name == 'DECODE':
-    #     os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_device_num)
 
     params = load_yaml_train(args.train_params_path)
     
     if args.model_name == 'LiteLoc':
-        from network.loc_model import LitelocModel
-        model = LitelocModel(params)
-    else:
-        from network.loc_model_decode import DECODEModel
-        model = DECODEModel(params)
+        from network.loc_model import LocModel
 
-    # liteloc = LitelocModel(params)
+    else:
+        from network.loc_model_decode import LocModel
+
+
+    model = LocModel(params)
 
     show_sample_psf(psf_pars=params.PSF_model)
     show_train_img(image_num=4, camera_params=params.Camera, psf_params=params.PSF_model, train_params=params.Training)
